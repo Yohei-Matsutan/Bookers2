f9d5d8375778ff081162bdf390a0756b56ae0af1
Yohei-Matsutan <takeda.yohei.sp@alumni.tsukuba.ac.jp>
2021-12-10 07:48:55 +0000

    [Add]All frames

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9e7d35089c79aed5866fabb1ba25dd7d9024aae53979d57f87ccef6ec1cfe23b106c8a4f8525f658175af9e2559f07d7f938c4f53a4de1a36d6e8765404dfb77'
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'about' => 'homes#about', as: 'about'
 
   # なんとなくusersとbooksのネストはしないurl長くなるし
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
 
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
 end
